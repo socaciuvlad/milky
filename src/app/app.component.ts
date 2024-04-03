7f90fa9398932e5e60084fa180f9b595d60521d0
socaciuvlad <vladsocaciucatalin@gmail.com>
2024-04-03 14:20:51 +0300

    imported @ngxw/widgets library locally

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BoxBarComponent } from '../../../ng-components/projects/box-bar/src/public-api';
-import { WidgetsComponent } from '@ngxw/widgets';
+import {
+  BoxBarComponent,
+  WidgetsComponent,
+} from '../../../ng-components/projects/ngxw/widgets/src/public-api';
+// import { WidgetsComponent } from '@ngxw/widgets';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BoxBarComponent, WidgetsComponent],
+  imports: [RouterOutlet, WidgetsComponent, BoxBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
