833e0267c3d256f8b6e69e6432370bee844ece70
Mihai Savin <mihai.savin@qiagen.com>
2024-04-08 18:06:46 +0300

    remove local testing stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {
-  BoxBarComponent,
-  WidgetsComponent,
-} from '../../../ng-components/projects/ngxw/widgets/src/public-api';
-// import { WidgetsComponent } from '@ngxw/widgets';
+// for local testing only
+// import {
+//   BoxBarComponent,
+//   WidgetsComponent,
+// } from '../../../ng-components/projects/ngxw/widgets/src/public-api';
+import { BoxBarComponent, WidgetsComponent, WidgetsModule } from '@ngxw/widgets';
 
 @Component({
   selector: 'app-root',
